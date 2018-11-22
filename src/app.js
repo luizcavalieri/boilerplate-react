816e67cd20250bf7a97ba336ab5ba0c28c208992
Luiz Cavalieri <luiz.cavalieri@gmail.com>
2018-11-22 12:23:49 +1100

    Update gitignore

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
+import './App.scss';
+import Button from './_/atoms/Button/Button';
 
 
-ReactDOM.render(<p>This is a boilerplate.</p>, document.getElementById('app'));
+
+const App = () => {
+ return (
+   <div className={'outer-container'}>
+     <p>This is a new boilerplate.</p>
+     <Button
+       label={'test'}
+
+     />
+   </div>
+ )
+};
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
